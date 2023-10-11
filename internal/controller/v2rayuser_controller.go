5f5c1a55ace86c00daa47be745535096ce103cb1
蛮三 <45847340+dusty-cjh@users.noreply.github.com>
2023-10-11 17:21:04 +0800

    fix v2ray user rbac bug

--- internal/controller/v2rayuser_controller.go
+++ internal/controller/v2rayuser_controller.go
@@ -80,6 +80,7 @@ type V2rayUserReconciler struct {
 //+kubebuilder:rbac:groups=core,resources=pods,verbs=get;list;watch
 //+kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=core,resources=pods,verbs=get;list;watch
+//+kubebuilder:rbac:groups=core,resources=nodes,verbs=get;list;watch
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
